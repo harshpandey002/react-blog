b240b071c01a40dca733e967a88aa233799e7284
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-05-30 23:48:04 +0530

    Action Ccomponent

--- client/src/components/blog/Action/Action.js
+++ client/src/components/blog/Action/Action.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./Action.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import { faHeart, faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
 
 function Action() {
   return (
     <div className="Action">
-      <h2>Action Component</h2>
+      <div className="btn">
+        <div className="btn--icon">
+          <FontAwesomeIcon icon={faHeart} />
+        </div>
+        <div className="btn--name">Add to Fav</div>
+      </div>
+      <div className="btn">
+        <div className="btn--icon">
+          <FontAwesomeIcon icon={faPen} />
+        </div>
+        <div className="btn--name">Edit Blog</div>
+      </div>
+      <div className="btn">
+        <div className="btn--icon">
+          <FontAwesomeIcon icon={faTrash} />
+        </div>
+        <div className="btn--name">Delete Blog</div>
+      </div>
     </div>
   );
 }
