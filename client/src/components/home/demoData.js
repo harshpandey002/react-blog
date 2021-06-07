cfc58a48933d188cf3b723914480adde9d4c7445
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-06-08 03:20:44 +0530

    Demo Data Update

--- client/src/components/home/demoData.js
+++ client/src/components/home/demoData.js
@@ -10,26 +10,28 @@ export const demoData = [
   },
   {
     id: "2",
-    title: "Demo Title 2",
-    description: "Demo description 2",
-    category: "health",
+    title: "How to Gain Intuition and Learn Fast",
+    description:
+      "Tapping into your brain’s powerful “procedural learning system” can help you develop intuitive problem solv...",
+    category: "programming",
     blogData: "<h1>Demo Story 2</h1>",
     created: Date(),
     like: false,
   },
   {
     id: "3",
-    title: "Demo Title 3",
-    description: "Demo description 3",
-    category: "programming",
+    title: "A Room Full of Change",
+    description: "Overcoming the difficulties of my newfound living situation.",
+    category: "health",
     blogData: "<h1>Demo Story 3</h1>",
     created: Date(),
     like: false,
   },
   {
     id: "4",
-    title: "Demo Title 4",
-    description: "Demo description 4",
+    title: "The Healthiest People in the World Don’t Go to the Gym",
+    description:
+      "Go ahead and cancel your membership, you already have the tools you need",
     category: "health",
     blogData: "<h1>Demo Story 4</h1>",
     created: Date(),
@@ -37,8 +39,9 @@ export const demoData = [
   },
   {
     id: "5",
-    title: "Demo Title 5",
-    description: "Demo description 5",
+    title: "Journey of Improving React App Performance by 10x",
+    description:
+      "Ever got this “Aw! Snap” on your application? Tried to solve it? Did you just google it and find no...",
     category: "programming",
     blogData: "<h1>Demo Story 5</h1>",
     created: Date(),
@@ -46,8 +49,9 @@ export const demoData = [
   },
   {
     id: "6",
-    title: "Demo Title 6",
-    description: "Demo description 6",
+    title: "Infinite Scrolling with Svelte",
+    description:
+      "loading more results on scroll with sveltet JS and restful APIs",
     category: "programming",
     blogData: "<h1>Demo Story 6</h1>",
     created: Date(),
@@ -55,8 +59,9 @@ export const demoData = [
   },
   {
     id: "7",
-    title: "Demo Title 7",
-    description: "Demo description 7",
+    title: "4 Habits Everyone Needs for Better Mental Health",
+    description:
+      "You are what you habitually do. Mental health is a frequently misunderstood topic.",
     category: "health",
     blogData: "<h1>Demo Story 7</h1>",
     created: Date(),
@@ -64,8 +69,9 @@ export const demoData = [
   },
   {
     id: "8",
-    title: "Demo Title 8",
-    description: "Demo description 8",
+    title: "3 Habits of Incredibly Healthy People",
+    description:
+      "No, you don’t have to go to the gym.I would consider myself a generally happy individual I exercise regula...",
     category: "health",
     blogData: "<h1>Demo Story 8</h1>",
     created: Date(),
@@ -73,8 +79,9 @@ export const demoData = [
   },
   {
     id: "9",
-    title: "Demo Title 9",
-    description: "Demo description 9",
+    title: "The setState Hook for Setting State on an Array of Children",
+    description:
+      "Dynamically creating children and giving them each an event listener is common. These event liste...",
     category: "programming",
     blogData: "<h1>Demo Story 9</h1>",
     created: Date(),
@@ -82,8 +89,9 @@ export const demoData = [
   },
   {
     id: "10",
-    title: "Demo Title 10",
-    description: "Demo description 10",
+    title: "For a Happier, Healthier 2021, Ditch These 6 Habits.",
+    description:
+      "After a tough year, plenty of us are amped about turning the page. Trouble is, Covid’s not done w...",
     category: "health",
     blogData: "<h1>Demo Story 10</h1>",
     created: Date(),
@@ -91,8 +99,9 @@ export const demoData = [
   },
   {
     id: "11",
-    title: "Demo Title 11",
-    description: "Demo description 11",
+    title: "What I Learned from Bullfighting",
+    description:
+      "Bullfighting was not on my “To-Do List.” It wasn’t even on my “Bucket List.” But somehow, I found myself at...",
     category: "sports",
     blogData: "<h1>Demo Story 11</h1>",
     created: Date(),
@@ -100,8 +109,9 @@ export const demoData = [
   },
   {
     id: "12",
-    title: "Demo Title 12",
-    description: "Demo description 12",
+    title: "Create a Responsive Grid With React, Flexbox & Styled-Components",
+    description:
+      "Don’t bother with CSS frameworks. Create responsive grid systems quickly with react, flexbox and styled...",
     category: "programming",
     blogData: "<h1>Demo Story 12</h1>",
     created: Date(),
@@ -109,8 +119,9 @@ export const demoData = [
   },
   {
     id: "13",
-    title: "Demo Title 13",
-    description: "Demo description 13",
+    title: "A Skydive Changed my Life",
+    description:
+      "On1 November 2014 I ‘fell’ (you don’t actually jump) out of a perfectly good plane.",
     category: "sports",
     blogData: "<h1>Demo Story 13</h1>",
     created: Date(),
@@ -118,8 +129,9 @@ export const demoData = [
   },
   {
     id: "14",
-    title: "Demo Title 14",
-    description: "Demo description 14",
+    title: "Every Company Should Offer Mental Health Days",
+    description:
+      "There wasn’t anything particularly “hard” about today. I woke up, I showered, I folded some clothe...",
     category: "health",
     blogData: "<h1>Demo Story 14</h1>",
     created: Date(),
@@ -127,8 +139,9 @@ export const demoData = [
   },
   {
     id: "15",
-    title: "Demo Title 15",
-    description: "Demo description 15",
+    title: "Is Lebron the Basketball GOAT?",
+    description:
+      "Now that LeBron James has won his fourth NBA title the conversation can begin in earnest. Is LeBron the (GO...",
     category: "sports",
     blogData: "<h1>Demo Story 15</h1>",
     created: Date(),
@@ -136,8 +149,9 @@ export const demoData = [
   },
   {
     id: "16",
-    title: "Demo Title 16",
-    description: "Demo description 16",
+    title: "I Got Rejected by Apple Music… So I Redesigned It",
+    description:
+      "I’d like to preface this case study by stating that my intentions behind redesigning Apple Music were in no...",
     category: "entertainment",
     blogData: "<h1>Demo Story 16</h1>",
     created: Date(),
@@ -145,8 +159,9 @@ export const demoData = [
   },
   {
     id: "17",
-    title: "Demo Title 17",
-    description: "Demo description 17",
+    title: "What Kobe Bryant Knew About Winning, in Basketball",
+    description:
+      "He was talking about the fact that Kobe Bryant was big, but not that big. He was quick, but not that quick.",
     category: "sports",
     blogData: "<h1>Demo Story 17</h1>",
     created: Date(),
@@ -154,8 +169,9 @@ export const demoData = [
   },
   {
     id: "18",
-    title: "Demo Title 18",
-    description: "Demo description 18",
+    title: "7 signs you’re rich, even if it doesn’t feel like it",
+    description:
+      "“Rich” can be just as subjective as “happy” — it’s different for everyone. But there are still a few unive...",
     category: "business",
     blogData: "<h1>Demo Story 18</h1>",
     created: Date(),
@@ -163,8 +179,9 @@ export const demoData = [
   },
   {
     id: "19",
-    title: "Demo Title 19",
-    description: "Demo description 19",
+    title: "Background Music for Coding",
+    description:
+      "Do you guys use background music while coding? They say that the right music allows to catch the flow and i...",
     category: "entertainment",
     blogData: "<h1>Demo Story 19</h1>",
     created: Date(),
@@ -172,8 +189,9 @@ export const demoData = [
   },
   {
     id: "20",
-    title: "Demo Title 20",
-    description: "Demo description 20",
+    title: "So, Let’s Build A Professional Soccer Team",
+    description:
+      "A manifesto on the intersection of startups and lower-level soccer that ends with the United States winni...",
     category: "sports",
     blogData: "<h1>Demo Story 20</h1>",
     created: Date(),
@@ -181,8 +199,9 @@ export const demoData = [
   },
   {
     id: "21",
-    title: "Demo Title 21",
-    description: "Demo description 21",
+    title: "Why business fail at machine learning",
+    description:
+      "I’d like to let you in on a secret: when people say ‘machine learning’ it sounds like there’s only one dis...",
     category: "business",
     blogData: "<h1>Demo Story 21</h1>",
     created: Date(),
@@ -190,8 +209,9 @@ export const demoData = [
   },
   {
     id: "22",
-    title: "Demo Title 22",
-    description: "Demo description 22",
+    title: "A 15-minute guide to getting your life back.",
+    description:
+      "How training my brain to focus helped me to build two profitable businesses",
     category: "business",
     blogData: "<h1>Demo Story 22</h1>",
     created: Date(),
@@ -199,8 +219,9 @@ export const demoData = [
   },
   {
     id: "23",
-    title: "Demo Title 23",
-    description: "Demo description 23",
+    title: "Analyze a Soccer game using Tensorflow Object",
+    description:
+      "The world cup season is here and off to an interesting start. Who ever thought the reining champions Germa...",
     category: "sports",
     blogData: "<h1>Demo Story 23</h1>",
     created: Date(),
@@ -208,8 +229,9 @@ export const demoData = [
   },
   {
     id: "24",
-    title: "Demo Title 24",
-    description: "Demo description 24",
+    title: "Prepare For The Death & Rebirth of Hollywood",
+    description:
+      "Production, exhibition, and good old fashion power are all being violently uprooted in tinsel town...",
     category: "entertainment",
     blogData: "<h1>Demo Story 24</h1>",
     created: Date(),
@@ -217,8 +239,9 @@ export const demoData = [
   },
   {
     id: "25",
-    title: "Demo Title 25",
-    description: "Demo description 25",
+    title: "IPL Betting Tips by Faizal Guru",
+    description:
+      "Betting comes in a grey area in many countries. They do not have well-defined laws or rules to regulate.",
     category: "sports",
     blogData: "<h1>Demo Story 25</h1>",
     created: Date(),
@@ -226,8 +249,9 @@ export const demoData = [
   },
   {
     id: "26",
-    title: "Demo Title 26",
-    description: "Demo description 26",
+    title: "Designers shouldn’t code. They should study business.",
+    description:
+      "ncreasingly, more and more companies are looking for great design leadership these days. They are bein...",
     category: "business",
     blogData: "<h1>Demo Story 26</h1>",
     created: Date(),
@@ -235,8 +259,9 @@ export const demoData = [
   },
   {
     id: "27",
-    title: "Demo Title 27",
-    description: "Demo description 27",
+    title: "Things You Learn After 1 Year of Day Trading for a Living",
+    description:
+      "I’ve made 20% ROI in 6 months and lost it all in a single month. Then I lost 30% in 10 trades the foll...",
     category: "entertainment",
     blogData: "<h1>Demo Story 27</h1>",
     created: Date(),
@@ -244,8 +269,9 @@ export const demoData = [
   },
   {
     id: "28",
-    title: "Demo Title 28",
-    description: "Demo description 28",
+    title: "Turning Fortnite into PUBG with Deep Learning (CycleGAN)",
+    description:
+      "Understanding CycleGAN for Image Style Transfer and exploring its application to graphics mods for games.",
     category: "entertainment",
     blogData: "<h1>Demo Story 28</h1>",
     created: Date(),
@@ -253,8 +279,9 @@ export const demoData = [
   },
   {
     id: "29",
-    title: "Demo Title 29",
-    description: "Demo description 29",
+    title: "7 things people who are good with money never buy",
+    description:
+      "People who are good with money look for quality over quantity, and don’t make purchases that will der...",
     category: "business",
     blogData: "<h1>Demo Story 29</h1>",
     created: Date(),
@@ -262,8 +289,9 @@ export const demoData = [
   },
   {
     id: "30",
-    title: "Demo Title 30",
-    description: "Demo description 30",
+    title: "Playing Video Games Is Killing You",
+    description:
+      "In early high school, most of my time was spent playing video games. Most of my memories from th...",
     category: "entertainment",
     blogData: "<h1>Demo Story 30</h1>",
     created: Date(),
