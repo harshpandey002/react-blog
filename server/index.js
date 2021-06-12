9fe98bc285afe3353501b6053bd4f7304aab3d66
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-06-12 22:56:29 +0530

    Connecting server to DB

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://reactmedium:reactmedium1679@cluster0.wns4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
+//
