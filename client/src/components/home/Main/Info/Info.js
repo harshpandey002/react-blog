5fa174ee654c5dd94608b70d08b6a020acbc0728
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-06-15 02:47:18 +0530

    Info Component

--- client/src/components/home/Main/Info/Info.js
+++ client/src/components/home/Main/Info/Info.js
@@ -1,8 +1,45 @@
 import React from "react";
 import "./Info.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
 function Info() {
-  return <div className="Info"></div>;
+  return (
+    <div className="Info">
+      <FontAwesomeIcon
+        onClick={() =>
+          window.open("https://github.com/harshpandey002/react-blog/", "_blank")
+        }
+        className="brands"
+        size="2x"
+        icon={faGithub}
+      />
+      <FontAwesomeIcon
+        onClick={() =>
+          window.open(
+            "https://www.linkedin.com/in/harsh-kumar-pandey-5ab9071aa/",
+            "_blank"
+          )
+        }
+        className="brands"
+        size="2x"
+        icon={faLinkedin}
+      />
+      <FontAwesomeIcon
+        onClick={() =>
+          window.open("https://www.instagram.com/harshpandey_002/", "_blank")
+        }
+        className="brands"
+        size="2x"
+        icon={faInstagram}
+      />
+    </div>
+  );
 }
 
 export default Info;
